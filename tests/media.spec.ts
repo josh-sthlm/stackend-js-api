f6f0b9cc30ef564cbb61b4a597f0094f14c5928d
jens <jens@josh.se>
2020-08-10 15:38:19 +0200

    More tests

---
+++ tests/media.spec.ts
@@ -0,0 +1,57 @@
+//@flow
+
+import createTestStore from './setup-redux';
+import { get, GetMediaResult, list, ListResult, MediaListOrder, ThumbnailSize } from '../src/media'
+import { COMMUNITY_PARAMETER } from '../src/api'
+import { STACKEND_COM_COMMUNITY_PERMALINK } from '../src/stackend'
+import { DEFAULT_CMS_CONTEXT } from '../src/cms'
+
+
+describe('Media', () => {
+  let store = createTestStore();
+
+  describe("getMedia", () => {
+    it("Gets a media object", async () => {
+      let r: GetMediaResult = await store.dispatch(get({
+        id: 2,
+        context: DEFAULT_CMS_CONTEXT,
+        thumbnailConfigName: ThumbnailSize.MEDIUM,
+        [COMMUNITY_PARAMETER]: STACKEND_COM_COMMUNITY_PERMALINK
+      }));
+
+      expect(r.__resultCode).toBe("success");
+      expect(r.media).toBeDefined();
+      expect(r.media.id).toBe(2);
+      expect(r.html).toBe("<img src=\"https://api.stackend.com/media/get/c55/cms/developers-jpg.jpg\" alt=\"\" width=\"1250\" height=\"631\" style=\"max-width:1250px;\" class=\"stackend-responsive\"/>");
+      expect(r.thumbnail).toBeDefined();
+      expect(r.thumbnail.mediaId).toBe(2);
+      expect(r.thumbnail.url).toBeDefined();
+    })
+  });
+
+	describe('list', () => {
+		it("List media files", async () => {
+
+      let r:ListResult = await store.dispatch(list({
+        context: DEFAULT_CMS_CONTEXT,
+        pageSize: 2,
+        order: MediaListOrder.CREATED_ASC,
+        [COMMUNITY_PARAMETER]: STACKEND_COM_COMMUNITY_PERMALINK
+      }));
+
+      expect(r.__resultCode).toBe("success");
+      expect(r.mediaPaginated).toBeDefined();
+      expect(r.thumbnailsByMediaId).toBeDefined();
+      expect(r.mediaPaginated.totalSize).toBeGreaterThanOrEqual(1);
+      expect(r.mediaPaginated.entries).toBeDefined();
+
+      let m = r.mediaPaginated.entries[0];
+      expect(m).toBeDefined();
+      expect(m.id).toBe(2);
+		})
+	});
+
+
+});
+
+
--- tests/stackend.spec.ts
+++ tests/stackend.spec.ts
@@ -1,7 +1,11 @@
 //@flow
 
 import createTestStore from './setup-redux';
-import { getCommunity, GetCommunityResult, STACKEND_COM_COMMUNITY_PERMALINK } from '../src/stackend'
+import {
+  getCommunity,
+  GetCommunityResult, getReferenceUrl, isCommunityUrlBlocked,
+  STACKEND_COM_COMMUNITY_PERMALINK
+} from '../src/stackend'
 
 describe('Stackend', () => {
   let store = createTestStore();
@@ -16,7 +20,28 @@ describe('Stackend', () => {
       expect(c.id).toBe(55);
       expect(c.permalink).toBe(STACKEND_COM_COMMUNITY_PERMALINK);
 		})
-	})
+	});
+
+	describe("isCommunityUrlBlocked", () => {
+	  it("Check if a community permalink is allowed or not", () => {
+      expect(isCommunityUrlBlocked("facebook")).toBeTruthy();
+      expect(isCommunityUrlBlocked("google")).toBeTruthy();
+      expect(isCommunityUrlBlocked("user")).toBeTruthy();
+      expect(isCommunityUrlBlocked("register")).toBeTruthy();
+      expect(isCommunityUrlBlocked("create")).toBeTruthy();
+      expect(isCommunityUrlBlocked("stacks")).toBeTruthy();
+      expect(isCommunityUrlBlocked("my-settings")).toBeTruthy();
+    })
+  });
+
+	describe("getReferenceUrl", () => {
+    it("Get the domain (excluding www) and path of an url", () => {
+      expect(getReferenceUrl("")).toBe(""); // FIXME: Improve this case
+      expect(getReferenceUrl("http://www.stackend.com/test")).toBe("stackend.com/test");
+      expect(getReferenceUrl("https://stackend.com/test")).toBe("stackend.com/test");
+    })
+  });
+
 });
 
 
