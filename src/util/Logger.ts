a78a356ada0d41a2fa0cc8c706f18a045fa08a44
jens <jens@josh.se>
2021-02-09 16:39:02 +0100

    Logging improvements

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -2,10 +2,19 @@
  * Interface for logging. A subset of the Console object.
  */
 export default interface Logger {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   log(message?: any, ...optionalParams: any[]): void;
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   debug(message?: any, ...optionalParams: any[]): void;
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   info(message?: any, ...optionalParams: any[]): void;
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   warn(message?: any, ...optionalParams: any[]): void;
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   error(message?: any, ...optionalParams: any[]): void;
 }
 
@@ -33,40 +42,46 @@ export class ConsoleLogger implements Logger {
     }
   }
 
-  setLevel(level: Level) {
+  setLevel(level: Level): void {
     this.level = level;
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   debug(message?: any, ...optionalParams: any[]): void {
     if (this.level <= Level.DEBUG) {
       console.debug(this.getMessage(message), ...optionalParams);
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   log(message?: any, ...optionalParams: any[]): void {
     if (this.level <= Level.INFO) {
       console.log(this.getMessage(message), ...optionalParams);
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   info(message?: any, ...optionalParams: any[]): void {
     if (this.level <= Level.INFO) {
       console.info(this.getMessage(message), ...optionalParams);
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   warn(message?: any, ...optionalParams: any[]): void {
     if (this.level <= Level.WARN) {
       console.warn(this.getMessage(message), ...optionalParams);
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   error(message?: any, ...optionalParams: any[]): void {
     if (this.level <= Level.ERROR) {
       console.error(this.getMessage(message), ...optionalParams);
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   getMessage(message?: any): string {
     return this.name ? this.name + ': ' + message : message;
   }
